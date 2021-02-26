66a6e4445039a16c79b09963f008d6562650bd0f
Shubhankar <das.shubhankar97@gmail.com>
2021-02-26 22:58:08 +0530

    added safe area view

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+import IconFeather from 'react-native-vector-icons/Feather';
+import IconMaterial from 'react-native-vector-icons/MaterialCommunityIcons';
 import {NavigationContainer} from '@react-navigation/native';
 import {navigationRef} from './navigation/RootNavigation';
 import navigationTheme from './navigation/navigationTheme';
 import AppNavigator from './navigation/AppNavigator';
+import {SafeAreaView, StatusBar, StyleSheet} from 'react-native';
+import colors from './constants/colors';
+
+IconFeather.loadFont();
+IconMaterial.loadFont();
 
 function App() {
   return (
-    <NavigationContainer ref={navigationRef} theme={navigationTheme}>
-      <AppNavigator />
-    </NavigationContainer>
+    <SafeAreaView style={styles.container}>
+      <NavigationContainer ref={navigationRef} theme={navigationTheme}>
+        <AppNavigator />
+      </NavigationContainer>
+      <StatusBar barStyle="light-content" />
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.black,
+  },
+});
 
 export default App;
